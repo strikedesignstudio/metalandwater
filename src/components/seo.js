8f9d9c2176d012402cd96d958aec846d3af94c44
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-02-26 12:32:40 -0500

    menu

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,8 +5,8 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
 function Seo({ description, title, keywords, image, children }) {
   const { site } = useStaticQuery(
@@ -27,29 +27,29 @@ function Seo({ description, title, keywords, image, children }) {
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
   const metaKeywords = keywords
-    ? site.siteMetadata?.keywords.concat(", ", keywords)
+    ? site.siteMetadata?.keywords.concat(', ', keywords)
     : site.siteMetadata?.keywords
 
   return (
     <>
       <title>{title ? `${title} | ${defaultTitle}` : `${defaultTitle}`}</title>
-      <meta name="description" content={metaDescription} />
-      <meta name="keywords" content={metaKeywords} />
+      <meta name='description' content={metaDescription} />
+      <meta name='keywords' content={metaKeywords} />
       <meta
-        property="og:title"
+        property='og:title'
         content={title ? `${title} | ${defaultTitle}` : `${defaultTitle}`}
       />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content={`https:${image}`} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+      <meta property='og:description' content={metaDescription} />
+      <meta property='og:type' content='website' />
+      <meta property='og:image' content={`https:${image}`} />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:creator' content={site.siteMetadata?.author || ``} />
       <meta
-        name="twitter:title"
+        name='twitter:title'
         content={title ? `${title} | ${defaultTitle}` : `${defaultTitle}`}
       />
-      <meta name="twitter:description" content={metaDescription} />
-      <meta property="twitter:image" content={`https:${image}`} />
+      <meta name='twitter:description' content={metaDescription} />
+      <meta property='twitter:image' content={`https:${image}`} />
       {children}
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Seo from '../components/seo'
+import Layout from '../components/layout'
 
 const Index = () => {
-  return (
-    <div>Index</div>
-  )
+  return <Layout>Index</Layout>
 }
+
+export const Head = () => <Seo />
 
 export default Index
