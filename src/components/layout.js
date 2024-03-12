bb4becd6fa091ddeca7d3bc7fa6463113c6099d6
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-03-12 08:47:03 -0400

    accessibility widget

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { Script } from 'gatsby'
 
 const Layout = ({ children, location }) => {
   return (
     <>
       <Header></Header>
       <main>{children}</main>
+      <Script
+        src='https://website-widgets.pages.dev/dist/sienna.min.js'
+        defer
+      ></Script>
       {location?.pathname !== '/' && <Footer></Footer>}
     </>
   )
