b6592955c1ce0b13ab32d4bd0ce955e867ced905
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2025-09-17 14:46:35 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,9 +24,9 @@ const About = ({ data }) => {
           {contact.map((contact) => (
             <div key={contact.id}>
               <p>{contact.name}</p>
-              <a href={`mailto:${contact.role}`}>{contact.role}</a>
-              <a href={`mailto:${contact.email}`}>{contact.email}</a>
-              <a href={`mailto:${contact.xemail}`}>{contact.xemail}</a>
+              <p><a href={`mailto:${contact.role}`}>{contact.role}</a></p>
+              <p><a href={`mailto:${contact.email}`}>{contact.email}</a></p>
+              <p><a href={`mailto:${contact.xemail}`}>{contact.xemail}</a></p>
             </div>
           ))}
         </div>
