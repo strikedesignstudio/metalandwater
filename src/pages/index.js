2815d432e701e4ecb49d46b21e7c6af81af02c79
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2025-12-30 09:29:39 +0000

    Refactor motion.div for visibility handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,20 +11,13 @@ const Index = () => {
     <>
       <Splash></Splash>
       <AnimatePresence>
-  {isVisible && (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.4 }}
-    >
-      <Layout>
-        <HomeSlider />
-        <Mission />
-      </Layout>
-    </motion.div>
-  )}
-</AnimatePresence>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <Layout>
+            <HomeSlider />
+            <Mission />
+          </Layout>
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
