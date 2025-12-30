eef741656ee776ace606fc76a6a5dcc1cc076c5c
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2025-12-30 09:28:27 +0000

    Add exit animation to motion.div in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,20 @@ const Index = () => {
     <>
       <Splash></Splash>
       <AnimatePresence>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          <Layout>
-            <HomeSlider />
-            <Mission />
-          </Layout>
-        </motion.div>
-      </AnimatePresence>
+  {isVisible && (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.4 }}
+    >
+      <Layout>
+        <HomeSlider />
+        <Mission />
+      </Layout>
+    </motion.div>
+  )}
+</AnimatePresence>
     </>
   )
 }
