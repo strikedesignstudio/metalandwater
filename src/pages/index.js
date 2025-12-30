c01dc39c4e2dbc20f826c9f05827357fab12af77
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2025-12-30 09:31:49 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,30 +6,7 @@ import Splash from '../components/splash'
 import Mission from '../components/mission'
 import { AnimatePresence, motion } from 'framer-motion'
 
-const Index = () => {
-  const [showContent, setShowContent] = useState(false)
-
-  return (
-    <>
-      <Splash onFinish={() => setShowContent(true)} />
-
-      <AnimatePresence>
-        {showContent && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Layout>
-              <HomeSlider />
-              <Mission />
-            </Layout>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
-  )
-}
+const Index = () => { return ( <> <Splash></Splash> <AnimatePresence> <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}> <Layout> <HomeSlider /> <Mission /> </Layout> </motion.div> </AnimatePresence> </> ) }
 
 export const Head = () => <Seo />
 
