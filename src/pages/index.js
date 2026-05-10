c5288d7463761f7da1e7d28289d5e2406551bac3
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2026-05-10 19:18:49 +0100

    Refactor Index component to remove Splash and AnimatePresence

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Seo from '../components/seo'
 import Layout from '../components/layout'
 import HomeSlider from '../components/homeSlider'
-import Splash from '../components/splash'
 import Mission from '../components/mission'
-import { AnimatePresence, motion } from 'framer-motion'
 
-const Index = () => { return ( <> <Splash></Splash> <AnimatePresence> <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}> <Layout> <HomeSlider /> <Mission /> </Layout> </motion.div> </AnimatePresence> </> ) }
+const Index = () => {
+  return (
+    <Layout>
+      <HomeSlider />
+      <Mission />
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo />
-
 export default Index
