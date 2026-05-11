5e8f523cf98ea1a52451242a132270506739e8d5
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2026-05-11 08:55:07 +0100

    Update artistListing.js

--- src/components/artistListing.js
+++ src/components/artistListing.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ArtistListing = ({ artist }) => {
   return (
-    <span className='artist-listing-item'>
+    <a href={artist.slug} target='_blank' rel='noreferrer' className='artist-listing-item' >
       {artist.artist}
-    </span>
+  </a>
   )
 }
 
