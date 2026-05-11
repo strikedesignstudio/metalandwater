569fa9fcb9a8b5bc76dca77b51d94197885b4cbc
strikedesignstudio <89917147+strikedesignstudio@users.noreply.github.com>
2026-05-11 08:32:19 +0100

    Refactor ArtistListing component to remove tooltip
    Removed tooltip functionality and related state management.

--- src/components/artistListing.js
+++ src/components/artistListing.js
@@ -1,32 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
 
-const ArtistListing = ({artist}) => {
- const [active, setActive] = useState(false)
-
- const showTip = () => {
-     setActive(true) 
- }
-
- const hideTip = () => {
-   setActive(false)
- }
+const ArtistListing = ({ artist }) => {
   return (
     <Link
       to={`/artist/${artist.slug}`}
       className='artist-listing-item'
-      onMouseEnter={showTip}
-      onMouseLeave={hideTip}
     >
       {artist.artist}
-      {active && (
-        <Link to={`/artist/${artist.slug}`} className='artist-tooltip'>
-          <GatsbyImage
-            image={artist.featuredImage.image?.gatsbyImageData}
-          ></GatsbyImage>
-        </Link>
-      )}
     </Link>
   )
 }
